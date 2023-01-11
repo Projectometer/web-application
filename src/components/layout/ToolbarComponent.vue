f47838ca812e05b44f8b0b4381829c2429cdcaa3
momicoder <112393289+momicoder@users.noreply.github.com>
2023-01-11 20:15:47 +0100

    LoginPage

---
+++ src/components/layout/ToolbarComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+    <q-tabs inline-label v-if="userData === null">
+      <!--  <q-route-tab icon="event" to="/Calendar" replace label="Timer" /> -->
+      <q-route-tab class="loginStyle btn noselect" name="Login" icon="fingerprint" to="/Login" replace label="Login" />
+    </q-tabs>
+
+    <q-tabs inline-label v-if="userData !== null">
+
+      <!--  <q-route-tab icon="event" to="/Calendar" replace label="Timer" /> -->
+      <q-route-tab class="loginStyle btn noselect" name="Logout" icon="fingerprint" to="/Logout" replace
+        label="Logout" />
+    </q-tabs>
+
+  </div>
+
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const userData = ref({});
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,14 +1,18 @@
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
+
+
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('src/layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/LoginPage.vue') },
+      { path: 'calendar', component: () => import('src/pages/TimerPage.vue') }
+
+    ],
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
