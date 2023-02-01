2829e5ee0f2b97ac7d586132100178ff80862414
momicoder <112393289+momicoder@users.noreply.github.com>
2023-02-01 19:08:19 +0100

    Update layouts

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,8 +1,10 @@
 <template>
   <q-page padding>
     <!-- content -->
+
   </q-page>
 </template>
 
 <script setup lang="ts">
+
 </script>
--- src/pages/TimerPage.vue
+++ src/pages/TimerPage.vue
@@ -5,14 +5,24 @@
     <div class="row ">
 
       <div class="col col col-1 calendar-padding bg-calendar-aside">
-        <div class="row calendar-padding">
-          Tools
+        <div class="row calendar-padding justify-center">
+
+          <q-tabs vertical>
+            <q-tab name="mails" icon="timer"></q-tab>
+            <q-tab name="alarms" icon="play_for_work"></q-tab>
+            <q-tab name="movies" icon="model_training"></q-tab>
+            <q-tab name="mails" icon="timer"></q-tab>
+            <q-tab name="alarms" icon="play_for_work"></q-tab>
+            <q-tab name="movies" icon="model_training"></q-tab>
+          </q-tabs>
+
         </div>
       </div>
 
       <div class="col col-11 calendar-padding bg-calendar-calendar">
-        <div class=" calendar-padding">
-          Q-Calendar
+        <div class="col calendar-padding">
+          <div class="row row1 clendar-header-style calendar-header-padding"></div>
+          <q-calendar class="row row12 calendar-calendar-padding"></q-calendar>
         </div>
       </div>
 
@@ -23,4 +33,5 @@
 </template>
 
 <script setup lang="ts">
+import { QCalendar } from '@quasar/quasar-ui-qcalendar'
 </script>
